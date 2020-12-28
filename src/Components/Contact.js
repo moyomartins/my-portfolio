fdec9e96dcae5d8ea5b83a0eba8ce205f50dcc99
unknown <martinstm@yahoo.com>
2020-12-28 12:53:48 -0800

    Dec 28

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -79,11 +79,10 @@ const Contact = ({ data }) => {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Contact</h4>
 					   <p className="address">
 						   {data?.name}<br />
-						   {data?.address.street} <br />
-						   {data?.address.city}, {data?.address.state} {data?.address.zip}<br />
+						   {data?.email} <br />
 						   <span>{data?.phone}</span>
 					   </p>
 				   </div>
