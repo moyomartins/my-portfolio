7c32a551aa964b02480ad24d2d6d7536fdcbe0a8
unknown <martinstm@yahoo.com>
2021-02-19 00:51:44 -0800

    email

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ const Contact = ({ data }) => {
    // const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
    const [name, setName] = useState('');
    const [subject, setSubject] = useState('');
-   const [email, setEmail] = useState('martinstm@yahoo.com');
+   const [email] = useState('martinstm@yahoo.com');
    const [message, setMessage] = useState('');
 
    console.log(data)
@@ -43,11 +43,6 @@ const Contact = ({ data }) => {
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input value={name} type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value={email} type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
                   </div>
 
                   <div>
